6d0eb306c81b9abc750103292119e428f599c0d5
Ammar <ammar.h.d@hotmail.com>
2021-03-24 21:51:02 +0100

    sitting l1 to l2 filter

--- app.js
+++ app.js
@@ -14,34 +14,50 @@ const csv = require("csv-parser");
 const server = http.createServer(app);
 
 const io = socketIo(server); // < Interesting!
+var trx1000 = [];
+var L1_transactions = [];
+
+///reading the file content
+fileStream = fs
+  .createReadStream("./output.csv")
+  .pipe(csv(" "))
+  .on("data", (row) => {
+    trx1000.push(Object.values(row));
+    if (trx1000.length >= 12000) {
+      fileStream.pause();
+    }
+  })
+  .on("end", () => {
+    console.log("CSV file successfully processed");
+  });
+
+// connecting the socket
 
 io.on("connection", (socket) => {
   socket.on("disconnect", () => {
     console.log(socket.id + " disconnected");
   });
 
-  var trx1000 = [];
+  //send l3 txs
+  setInterval(() => {
+    var l3_txs = L1_transactions;
+    L1_transactions = [];
+    socket.emit("l3_txs", l3_txs);
+  }, 1000);
 
   // set the timout interval
   socket.on("messege", () => {
+    //console.log(trx1000);
     socket.emit("FromAPI", trx1000);
     trx1000 = [];
     fileStream.resume();
   });
 
-  fileStream = fs
-    .createReadStream("./output.csv2")
-    .pipe(csv(" "))
-    .on("data", (row) => {
-      trx1000.push(Object.values(row));
-      if (trx1000.length >= 10000) {
-        fileStream.pause();
-        console.log(trx1000);
-      }
-    })
-    .on("end", () => {
-      console.log("CSV file successfully processed");
-    });
+  // listen to l1_transactions from reactjs
+  socket.on("l1_txs", (txs) => {
+    console.log(txs);
+    L1_transactions = [...L1_transactions, ...txs];
+  });
 });
 
 server.listen(port, () => console.log(`Listening on port ${port}`));
